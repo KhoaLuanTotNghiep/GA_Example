6bcf0018453656633dbdecf9634b06658a1e7ee0
huynhnhu1502 <chauhuynhnhu1502@gmail.com>
2016-10-12 15:04:38 +0700

    commit

--- GA_Example/Form1.Designer.cs
+++ GA_Example/Form1.Designer.cs
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_LoadFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lb_Path = new System.Windows.Forms.Label();
+            this.txt_Search = new System.Windows.Forms.TextBox();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.txt_Result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_LoadFile
+            // 
+            this.btn_LoadFile.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_LoadFile.Location = new System.Drawing.Point(9, 35);
+            this.btn_LoadFile.Name = "btn_LoadFile";
+            this.btn_LoadFile.Size = new System.Drawing.Size(95, 23);
+            this.btn_LoadFile.TabIndex = 0;
+            this.btn_LoadFile.Text = "Choose file...";
+            this.btn_LoadFile.UseVisualStyleBackColor = false;
+            this.btn_LoadFile.Click += new System.EventHandler(this.btn_LoadFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // lb_Path
+            // 
+            this.lb_Path.AutoSize = true;
+            this.lb_Path.Location = new System.Drawing.Point(120, 40);
+            this.lb_Path.Name = "lb_Path";
+            this.lb_Path.Size = new System.Drawing.Size(0, 14);
+            this.lb_Path.TabIndex = 1;
+            // 
+            // txt_Search
+            // 
+            this.txt_Search.Location = new System.Drawing.Point(10, 70);
+            this.txt_Search.Name = "txt_Search";
+            this.txt_Search.Size = new System.Drawing.Size(202, 20);
+            this.txt_Search.TabIndex = 2;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(234, 68);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(75, 23);
+            this.btn_Search.TabIndex = 3;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // txt_Result
+            // 
+            this.txt_Result.Enabled = false;
+            this.txt_Result.Location = new System.Drawing.Point(9, 135);
+            this.txt_Result.Multiline = true;
+            this.txt_Result.Name = "txt_Result";
+            this.txt_Result.Size = new System.Drawing.Size(830, 326);
+            this.txt_Result.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 14);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Result";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(851, 491);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Result);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.txt_Search);
+            this.Controls.Add(this.lb_Path);
+            this.Controls.Add(this.btn_LoadFile);
+            this.Name = "Form1";
+            this.Text = "Searching document program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_LoadFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lb_Path;
+        private System.Windows.Forms.TextBox txt_Search;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.TextBox txt_Result;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- GA_Example/Form1.cs
+++ GA_Example/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using btl.generic;
 
 namespace GA_Example
 {
@@ -15,5 +17,83 @@ namespace GA_Example
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_LoadFile_Click(object sender, EventArgs e)
+        {
+            //DialogResult result = openFileDialog1.ShowDialog();
+            //if (result == DialogResult.OK) // Test result.
+            //{
+                //Do whatever you want
+                //openFileDialog1.FileName .....
+            //}
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text files | *.txt"; // file types, that will be allowed to upload
+            dialog.Multiselect = false; // allow/deny user to upload more than one file at a time
+            if (dialog.ShowDialog() == DialogResult.OK) // if user clicked OK
+            {
+                String path = dialog.FileName; // get name of file
+                string content;
+                using (StreamReader reader = new StreamReader(new FileStream(path, FileMode.Open), new UTF8Encoding())) // do anything you want, e.g. read it
+                {
+                    lb_Path.Text = path;
+                    content = reader.ReadToEnd();
+                    txt_Result.Text = content;
+                }
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            GA ga = new GA(0.8, 0.05, 100, 2000, 2);
+
+            ga.FitnessFunction = new GAFunction(theActualFunction);
+
+            //ga.FitnessFile = @"H:\fitness.csv";
+            ga.Elitism = true;
+            ga.Go();
+
+            double[] values;
+            double fitness;
+            ga.GetBest(out values, out fitness);
+            System.Console.WriteLine("Best ({0}):", fitness);
+            for (int i = 0; i < values.Length; i++)
+                System.Console.WriteLine("{0} ", values[i]);
+
+            ga.GetWorst(out values, out fitness);
+            System.Console.WriteLine("\nWorst ({0}):", fitness);
+            for (int i = 0; i < values.Length; i++)
+                System.Console.WriteLine("{0} ", values[i]);
+
+            Console.ReadLine();
+        }
+
+        //  optimal solution for this is (0.5,0.5)
+	        public static double theActualFunction(double[] values)
+	        {
+		        if (values.GetLength(0) != 2)
+			        throw new ArgumentOutOfRangeException("should only have 2 args");
+
+		        double x = values[0];
+		        double y = values[1];
+		        double n = 9;  //  should be an int, but I don't want to waste time casting.
+
+		        double f1 = Math.Pow(15*x*y*(1-x)*(1-y)*Math.Sin(n*Math.PI*x)*Math.Sin(n*Math.PI*y),2);
+		        return f1;
+	        }
     }
 }
